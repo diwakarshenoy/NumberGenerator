72d86dd546e7df072a8a36c82f6904e2f5392694
diwakarshenoy <diwakar.shenoy@gmail.com>
2019-07-11 21:25:48 +0530

    updated file on branch b2

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         App obj = new App();
-        System.out.println("git changes-6: After update: Unique ID : " + obj.generateUniqueKey());
+        System.out.println("git changes on branch b2: After update: Unique ID : " + obj.generateUniqueKey());
     }
 
     public String generateUniqueKey(){
